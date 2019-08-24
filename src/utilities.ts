d49b8d7bf36f4e017dcae2f1780c29febd7f92ea
Anant Anand Gupta <aag@payglobal.me>
2019-08-24 05:50:24 +0530

    Added Function: SetNestedPropertyValue - bug fixed: GetNestedPropertyValue was not returning the value if the property in question was a boolean and has false value.

--- src/utilities.ts
+++ src/utilities.ts
@@ -4,14 +4,31 @@ class Utilities {
     let pathParts = propPath.split('.');
 
     return pathParts.reduce((prev, curr) => {
-      if (!!prev && !!prev[curr]) return (prev[curr]);
+      if (!!prev && prev.hasOwnProperty(curr)) return (prev[curr]);
       else return returnUndefined ? undefined : null;
     }, obj) as T;
+  }
+
+  public static setNestedPropertyValue(target, path, value): boolean {
+    if (typeof target !== 'object' || !path || typeof path !== 'string') {
+      return false;
+    }
+    let pathParts: Array<any> = path.split('.');
+    let pathNode: any = pathParts.shift();
+    if (pathParts.length >= 1) {
+      !target[pathNode] ? (isNaN(pathParts[0]) ? target[pathNode] = {} : target[pathNode] = []) : '';
+      Utilities.setNestedPropertyValue(target[pathNode], pathParts.join('.'), value);
+    } else {
+      target[pathNode] = value;
+    }
+    return target;
   }
 };
 
 const GetNestedPropertyValue = Utilities.getNestedPropertyValue;
+const SetNestedPropertyValue = Utilities.setNestedPropertyValue;
 
 export {
-  GetNestedPropertyValue
+  GetNestedPropertyValue,
+  SetNestedPropertyValue,
 }
